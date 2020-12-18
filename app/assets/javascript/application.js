94934dddb94a258bf6cc8156e88e52701e9ee106
Kazane <kzn0313@gmail.com>
2020-12-18 22:22:15 +0900

    install design template

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,3 +1,8 @@
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//=require jquery/jquery.min
+//=require bootstrap/js/bootstrap.bundle.min
+//=require jquery.easing/jquery.easing.min
+//=require php-email-form/validate
+//=require aos/aos
+//=require isotope-layout/isotope.pkgd.min
+//=require owl.carousel/owl.carousel.min
+//=require main
