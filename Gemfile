05f9581c2221fccd6925c957ae9bd13131f56c19
Kazane <kzn0313@gmail.com>
2020-12-22 22:20:37 +0900

    add log in / log out function
    ログイン／ログアウト機能とUserテーブルを作りました。

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'kaminari'
 gem 'ransack'
+gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :works
   resources :directors
-  root to: 'home#index'
-  get 'home/index' => 'home#index'
+  root to: 'works#index'
+  # get 'home/index' => 'home#index'
 end
