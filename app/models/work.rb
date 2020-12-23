b19650e49a710019e139180befb07649830f7905
Kazane <kzn0313@gmail.com>
2020-12-23 22:08:25 +0900

    add position function

--- app/models/work.rb
+++ app/models/work.rb
@@ -12,4 +12,5 @@ class Work < ApplicationRecord
   scope :by_alphabet, ->{ order(title: :asc) }
   scope :by_position, ->{ order(position: :asc) }
   # scope :by_new, ->{ order(updated_at: :desc) }
+  acts_as_list
 end
